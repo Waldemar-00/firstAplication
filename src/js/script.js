bd65df90a6d6f817101ca1f4bda4cce962b985c1
waldemar <vova108vova@gmail.com>
2022-09-24 15:05:15 +0300

    app for movies

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+const numbersOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const oneFromLastFilm = prompt('Один из последних просмотренных фильмов?', ''); 
+const whatGrageFilm = prompt('На сколько оцените его?', '');
+const oneFromLastFilm2 = prompt('Один из последних просмотренных фильмов?', '');
+const whatGrageFilm2 = prompt('На сколько оцените его?', '');
+
+const personalMovieDB = {
+    count: numbersOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+
+};
+personalMovieDB.movies[oneFromLastFilm] = whatGrageFilm;
+personalMovieDB.movies[oneFromLastFilm2] = whatGrageFilm2;
+
+console.log(personalMovieDB);
