658c1c44bc4edb19ac82bfec0f2d69daa0b7265d
waldemar <vova108vova@gmail.com>
2022-09-26 17:12:32 +0300

    second app changes

--- src/js/script.js
+++ src/js/script.js
@@ -24,26 +24,31 @@ let oneFromLastFilm = '';
 let oneFromLastFilm2 = '';
 
 loop1:
-for(let i = 0; i < 10; i++) {
+for(let i = 0; i < 1; i++) {
     numbersOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-    if(numbersOfFilms === '' || numbersOfFilms === null )continue loop1;
-    else break;
+    if(numbersOfFilms === '' || numbersOfFilms === null ) {
+        --i;
+        continue loop1;
+    }else break;
 }
 
 loop12:
-for(let i = 0; i < 10; i++) { 
+for(let i = 0; i < 1; i++) { 
     oneFromLastFilm = prompt('Один из последних просмотренных фильмов?', '');
-    if(oneFromLastFilm === '' || oneFromLastFilm === null | oneFromLastFilm.length > 50)continue loop12;
-    else break;
+    if(oneFromLastFilm === '' || oneFromLastFilm === null || oneFromLastFilm.length > 50) {
+        --i;
+        continue loop12;
+    }else break;
 }
 const whatGrageFilm = prompt('На сколько оцените его?', '');
 
 loop21:
-for(let i = 0; i < 10; i++) {
+for(let i = 0; i < 1; i++) {
     oneFromLastFilm2 = prompt('Один из последних просмотренных фильмов?', '');
-    if(oneFromLastFilm2 === '' || oneFromLastFilm2 === null || oneFromLastFilm2.length > 50) 
-    continue loop21;
-    else break;
+    if(oneFromLastFilm2 === '' || oneFromLastFilm2 === null || oneFromLastFilm2.length > 50) {
+        --i;
+        continue loop21;
+    }else break;
 } 
 const whatGrageFilm2 = prompt('На сколько оцените его?', '');
 
@@ -58,7 +63,29 @@ const personalMovieDB = {
 if(personalMovieDB.count < 10)alert("Просмотрено довольно мало фильмов");
 else if(personalMovieDB.count <= 30)alert("Вы классический зритель");
 else if(personalMovieDB.count > 30 )alert("Вы киноман");
+else alert('Произошла ошибка');
 personalMovieDB.movies[oneFromLastFilm] = whatGrageFilm;
 personalMovieDB.movies[oneFromLastFilm2] = whatGrageFilm2;
 console.log(personalMovieDB);
 
+
+
+// 'use strict';
+// const numbersOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+// const oneFromLastFilm = prompt('Один из последних просмотренных фильмов?', ''); 
+// const whatGrageFilm = prompt('На сколько оцените его?', '');
+// const oneFromLastFilm2 = prompt('Один из последних просмотренных фильмов?', '');
+// const whatGrageFilm2 = prompt('На сколько оцените его?', '');
+// 
+// const personalMovieDB = {
+    // count: numbersOfFilms,
+    // movies: {},
+    // actors: {},
+    // genres: [],
+    // privat: false,
+// 
+// };
+// personalMovieDB.movies[oneFromLastFilm] = whatGrageFilm;
+// personalMovieDB.movies[oneFromLastFilm2] = whatGrageFilm2;
+// 
+// console.log(personalMovieDB);
