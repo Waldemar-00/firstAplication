6828f5ff530b09524f23a91ec752c28b4caeb54a
waldemar <vova108vova@gmail.com>
2022-09-29 22:22:20 +0300

    all conditions in functions

--- src/js/script.js
+++ src/js/script.js
@@ -19,39 +19,10 @@
 // console.log(personalMovieDB);
 
 'use strict';
-let numbersOfFilms = '';
-let oneFromLastFilm = '';
-let oneFromLastFilm2 = '';
-
-loop1:
-for(let i = 0; i < 1; i++) {
-    numbersOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-    if(numbersOfFilms === '' || numbersOfFilms === null ) {
-        --i;
-        continue loop1;
-    }else break;
-}
-
-loop12:
-for(let i = 0; i < 1; i++) { 
-    oneFromLastFilm = prompt('Один из последних просмотренных фильмов?', '');
-    if(oneFromLastFilm === '' || oneFromLastFilm === null || oneFromLastFilm.length > 50) {
-        --i;
-        continue loop12;
-    }else break;
-}
-const whatGrageFilm = prompt('На сколько оцените его?', '');
-
-loop21:
-for(let i = 0; i < 1; i++) {
-    oneFromLastFilm2 = prompt('Один из последних просмотренных фильмов?', '');
-    if(oneFromLastFilm2 === '' || oneFromLastFilm2 === null || oneFromLastFilm2.length > 50) {
-        --i;
-        continue loop21;
-    }else break;
-} 
-const whatGrageFilm2 = prompt('На сколько оцените его?', '');
-
+let numbersOfFilms;
+let oneFromLastFilm;
+let oneFromLastFilm2;
+let whatGrageFilm;
 const personalMovieDB = {
     count: numbersOfFilms,
     movies: {},
@@ -60,13 +31,65 @@ const personalMovieDB = {
     privat: false,
 
 };
-if(personalMovieDB.count < 10)alert("Просмотрено довольно мало фильмов");
-else if(personalMovieDB.count <= 30)alert("Вы классический зритель");
-else if(personalMovieDB.count > 30 )alert("Вы киноман");
-else alert('Произошла ошибка');
-personalMovieDB.movies[oneFromLastFilm] = whatGrageFilm;
-personalMovieDB.movies[oneFromLastFilm2] = whatGrageFilm2;
-console.log(personalMovieDB);
+function questionAboutAmountOfFilms() {
+    loop1:
+for(let i = 0; i < 1; i++) {
+    numbersOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+    personalMovieDB.count = numbersOfFilms;
+    if(numbersOfFilms === '' || numbersOfFilms === null ) {
+        --i;
+        continue loop1;
+    }else break;
+}
+return numbersOfFilms;
+}
+questionAboutAmountOfFilms();
+
+function questionAboutLastOfFilms() {
+    for (let i = 0; i < 2; i++) {
+        loop12:
+    for(let i = 0; i < 1; i++) { 
+    oneFromLastFilm = prompt('Один из последних просмотренных фильмов?', '');
+    whatGrageFilm = prompt('На сколько оцените его?', '');
+    personalMovieDB.movies[oneFromLastFilm] = whatGrageFilm;
+    if(oneFromLastFilm === '' || oneFromLastFilm === null || oneFromLastFilm.length > 50 || whatGrageFilm === '' || whatGrageFilm === null) {
+        --i;
+        continue loop12;
+    }else break;
+}
+    }
+return oneFromLastFilm, whatGrageFilm;
+}
+questionAboutLastOfFilms();
+
+function answerForViewer()  {
+    if(personalMovieDB.count < 10)alert("Просмотрено довольно мало фильмов");
+    else if(personalMovieDB.count <= 30)alert("Вы классический зритель");
+    else if(personalMovieDB.count > 30 )alert("Вы киноман");
+    else alert('Произошла ошибка');
+}
+answerForViewer();
+
+function showMyDB() {
+    if(!personalMovieDB.privat) {
+        console.log(personalMovieDB);
+    } 
+}
+showMyDB();
+
+function writeYourGenres() {
+    for(let i = 1; i < 4; i++) {
+        let whatGenresDoYouLike = prompt(`Ваш любимый жанр под номером ${i}`, '');
+        personalMovieDB.genres.push(whatGenresDoYouLike);
+    }
+        return personalMovieDB.genres;
+}
+alert(writeYourGenres());
+
+
+
+
+
 
 
 
