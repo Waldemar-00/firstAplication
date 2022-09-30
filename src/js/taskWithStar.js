227f23a93231609be895cfaa282573a361fe4e14
waldemar <vova108vova@gmail.com>
2022-10-01 00:06:56 +0300

    7 additional tasks with star)

--- src/js/taskWithStar.js
+++ src/js/taskWithStar.js
@@ -1,16 +1,79 @@
-function getValueAndSquareOfCube(edge) {
-    if(isNaN(edge) || edge <= 0 || edge % 1 !== 0)return "При вычислении произошла ошибка";
-    let  value = edge * edge * edge;
-    let  square = 6 * edge * edge;
-    let str = `Объем куба: ${value}, площадь всей поверхности: ${square}`;
-    return  str;
-}
-console.log(getValueAndSquareOfCube(5));
+// function getValueAndSquareOfCube(edge) {
+    // if(isNaN(edge) || edge <= 0 || edge % 1 !== 0)return "При вычислении произошла ошибка";
+    // let  value = edge * edge * edge;
+    // let  square = 6 * edge * edge;
+    // let str = `Объем куба: ${value}, площадь всей поверхности: ${square}`;
+    // return  str;
+// }
+// console.log(getValueAndSquareOfCube(5));
+// 
+// function getNumberOfCoup(ticketNumber) {
+    // // if(isNaN(ticketNumber) || ticketNumber < 0 || ticketNumber % 1 !== 0 )return "Ошибка. Проверьте правильность введенного номера места";
+    // else if(ticketNumber == 0 || ticketNumber > 36)return "Таких мест в вагоне не существует";
+    // const  coup = Math.ceil(ticketNumber / 4);
+    // return coup;
+// }
+// console.log(getNumberOfCoup(0));
 
-function getNumberOfCoup(ticketNumber) {
-    if(isNaN(ticketNumber) || ticketNumber < 0 || ticketNumber % 1 !== 0 )return "Ошибка. Проверьте правильность введенного номера места";
-    else if(ticketNumber == 0 || ticketNumber > 36)return "Таких мест в вагоне не существует";
-    const  coup = Math.ceil(ticketNumber / 4);
-    return coup;
+// function getTime(minutes) {
+    // if(minutes === 0)return  "Это 0 часов и 0 минут";
+    // if(isNaN(minutes) || minutes < 0 || minutes % 1 !== 0)return  "Ошибка, проверьте данные";
+    // let hour = Math.trunc(minutes / 60);
+    // let min = minutes - (hour * 60);
+    // if(hour === 1)return `Это ${hour} час и ${min} минут`;
+    // else if(hour === 2 || hour === 3 || hour === 4)return `Это ${hour} часа и ${min} минут`;
+    // return  `Это ${hour} часов и ${min} минут`;
+// }
+// console.log(getTime(550));
+
+// function geMaxNumber(a, b, c, d) {
+    // if(arguments.length < 4)return 0;
+    // for(let i  of arguments) {
+        // if(typeof i === 'string') {
+            // return 0;
+        // }
+    // }
+    // let maxNum = Math.max(a, b, c, d);
+    // return maxNum;
+// }
+// console.log(geMaxNumber( 1, 2, 3, 7 ));
+
+// function geMaxNumber(a, b, c, d) {
+    // if (typeof a === 'string' || a === undefined)return 0;
+    // if (typeof b === 'string' || b === undefined)return 0;
+    // if (typeof c === 'string' || c === undefined)return 0;
+    // if (typeof d === 'string' || d === undefined)return 0;
+    // let maxNum = Math.max(a, b, c, d);
+    // return maxNum;
+// }
+// console.log(geMaxNumber( 1, 2, 3, ));
+
+// function getFib(number) {
+    // let previousNumber = 0;
+    // let nextNumber = 1; 
+    // let str = '';
+    // for(let i = 0; i < number - 2; i++) {
+        // str += previousNumber + ' ' + nextNumber + ' ';
+        // previousNumber = previousNumber + nextNumber;
+        // nextNumber =  previousNumber + nextNumber;
+    // }
+    // return str;
+// }
+// console.log(getFib(5));
+
+function getFib(number) {
+    if(isNaN(number) || number % 1 !== 0)return '';
+    let previousNumber = 0;
+    let nextNumber = 1; 
+    let str = '';
+    for(let i = 0; i < number; i++) {
+        if(i === number - 1)str += previousNumber;
+        else str += previousNumber + ' ';
+        let temp = nextNumber;
+        nextNumber = previousNumber + nextNumber;
+        previousNumber = temp;
+        
+    }
+    return str;
 }
-console.log(getNumberOfCoup(0));
+console.log(getFib(1.5));
