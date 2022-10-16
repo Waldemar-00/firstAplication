e6c5facee666075d60d701810402aab1e34dbc39
waldemar <vova108vova@gmail.com>
2022-10-16 15:16:18 +0300

    added DOMMethods.html, ._._js

---
+++ src/js/DOMMethods.js
@@ -0,0 +1,14 @@
+alert('HELLO');
+const div = document.querySelector('div');
+const childrenDiv = div.children;
+console.log(childrenDiv.length);
+console.log(childrenDiv[0]);
+childrenDiv[0].remove();
+console.log(childrenDiv[0]);
+console.log('____________');
+
+
+
+
+
+
