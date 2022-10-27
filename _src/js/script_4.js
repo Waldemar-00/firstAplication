9e8ffc19cfdcefec918514044ec0980393b1275a
waldemar <vova108vova@gmail.com>
2022-10-27 21:29:20 +0300

    added script_4.js - Movie_Task

--- _src/js/script_4.js
+++ _src/js/script_4.js
@@ -1,15 +1,34 @@
 'use strict';
-const parentUl = document.querySelector('.promo__interactive-list');
-const aliveHtmlCollection = parentUl.children;
-// console.log('FUCK');
-// const basket = JSON.parse(JSON.stringify(Array.from(aliveHtmlCollection)));
-function addListener(arr) {
-    //! ARR is DEAD
-    arr.forEach((item, index) => {
-        item.addEventListener('click', () => {
+const parentUl = document.querySelector(".promo__interactive-list");
+const nodeListDead = document.querySelectorAll('.promo__interactive-item');
+const divDeleteDead = document.querySelectorAll('.delete');
 
-        });
-    });
+const input = document.querySelector(".adding__input");
+const button = document.querySelector("button");
+
+button.addEventListener("click", createNewMovieList);
+function createNewMovieList(event) {
+    event.preventDefault();
+    const viewedMovie = input.value;
+    createElement("li", `${viewedMovie}<div class="delete"></div>`, parentUl);
+    const nodeListDead = document.querySelectorAll(".promo__interactive-item");
+    const divDeleteDead = document.querySelectorAll(".delete");
+    addListener(Array.from(divDeleteDead), Array.from(nodeListDead));
+    // button.removeEventListener("click", createNewMovieList);
+}
+
+function deleteElement(arrEl, index) {
+    arrEl[index].remove();
+}
+function addListener(arrDel, arrEl) {
+    arrDel.forEach((item, index) => item.addEventListener("click", () => deleteElement(arrEl, index) ) );
+}
+addListener(Array.from(divDeleteDead), Array.from(nodeListDead));
+
+function createElement(el, string, parent) {
+    const elemLi = document.createElement(el);
+    elemLi.innerHTML = string;
+    elemLi.className = "promo__interactive-item";
+    parent.append(elemLi);
 
 }
-addListener(Array.from(aliveHtmlCollection));//! ALIVE
