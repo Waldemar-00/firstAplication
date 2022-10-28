8ba634635628a2d309b15532f24f9b4020fce595
waldemar <vova108vova@gmail.com>
2022-10-28 21:32:05 +0300

    added new  functions to script_4.js

--- _src/js/script_4.js
+++ _src/js/script_4.js
@@ -5,17 +5,19 @@ const divDeleteDead = document.querySelectorAll('.delete');
 
 const input = document.querySelector(".adding__input");
 const button = document.querySelector("button");
+const checkbox = document.querySelector('.checkbox');
 
-button.addEventListener("click", createNewMovieList);
 function createNewMovieList(event) {
     event.preventDefault();
-    const viewedMovie = input.value;
-    createElement("li", `${viewedMovie}<div class="delete"></div>`, parentUl);
+    checkInputValue();
+    if (checkbox.checked) createElement("li", `${input.value.bold()}<div class="delete"></div>`, parentUl);
+    else createElement("li", `${input.value}<div class="delete"></div>`, parentUl);
     const nodeListDead = document.querySelectorAll(".promo__interactive-item");
     const divDeleteDead = document.querySelectorAll(".delete");
     addListener(Array.from(divDeleteDead), Array.from(nodeListDead));
     // button.removeEventListener("click", createNewMovieList);
 }
+button.addEventListener("click", createNewMovieList);
 
 function deleteElement(arrEl, index) {
     arrEl[index].remove();
@@ -25,10 +27,17 @@ function addListener(arrDel, arrEl) {
 }
 addListener(Array.from(divDeleteDead), Array.from(nodeListDead));
 
+function checkInputValue() {
+    if(input.value.length > 22){ 
+        input.value = `${input.value.slice(0, 21)}...`;
+    } 
+    if(checkbox.checked)console.log(input.value);
+    return input.value;
+}
+
 function createElement(el, string, parent) {
     const elemLi = document.createElement(el);
     elemLi.innerHTML = string;
     elemLi.className = "promo__interactive-item";
     parent.append(elemLi);
-
 }
