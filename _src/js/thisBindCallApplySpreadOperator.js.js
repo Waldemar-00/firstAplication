6ff62533866ffa81c2f7c26f7a3377ffb15483f0
waldemar <vova108vova@gmail.com>
2022-11-02 19:59:06 +0300

    getter & setter

--- _src/js/thisBindCallApplySpreadOperator.js.js
+++ _src/js/thisBindCallApplySpreadOperator.js.js
@@ -17,3 +17,34 @@ obj.createStr.call(obj, ...['можно', 'купить']);//! passes scope & ca
 
 obj.createStr.apply(obj, ['можно', 'купить']); //! passes scope & call with array
 
+const numerObj = {
+    a: 1,
+    b: 2,
+    c: 3, 
+    d: 4,
+    e: 5,
+    get sum() {
+        return this.a + this.b + this.c + this.d + this.e;
+    },
+    set sum(e) {
+        this.e = e;
+    }
+}
+console.log(numerObj.sum);
+console.log(numerObj.sum = 10);
+console.log(numerObj.sum);
+
+const user = {
+    name: 'John',
+    surname: 'Pablo',
+    get fullName() {
+        return `${this.name} ${this.surname}`;
+    },
+    set fullName(string) {
+        [this.name, this.surname] = string.split(' ');
+    }
+}
+
+console.log(user.fullName);
+user.fullName = 'Alies Golg';
+console.log(user.fullName);
