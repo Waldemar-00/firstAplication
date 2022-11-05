2efb5f53ef0859b59e2e8bfd7483489e2097ceec
waldemar <vova108vova@gmail.com>
2022-11-05 18:39:23 +0300

    Symbol.iterator for Object

--- _src/js/generators.js
+++ _src/js/generators.js
@@ -27,32 +27,77 @@ console.log(iteration.next());// terminal condition is Infinity => done: true
 */
 //? GENERATOR
 
+// const objectIterator = {
+    // arr: ['first',
+            // 'second',
+            // 'third'  ],
+    // methodGenerator(array = this.arr) {
+        // let counter = 0;
+        // return {
+            // next() {
+                // if(counter < array.length) return {value: array[counter++],done: false};
+                // else return {value: array[counter],done: true};
+            // }
+        // }
+    // },
+    // getMethodGenerator(arr = this.arr) {
+        // iterator = objectIterator.methodGenerator(arr);
+        // for(let i = 0; i < arr.length; i++) {
+            // console.log(iterator.next().value);
+        // }
+    // return console.log(iterator.next());
+// }
+// }
+// const arr = [
+    // 'first',
+    // 'second',
+    // 'third',
+    // 'four',
+    // 'fifth'
+// ];
+// objectIterator.getMethodGenerator();
+
+//? Symbol.iterator
+
+// const objectIterator = {
+    // number: 10,
+    // [Symbol.iterator](num = this.number) {
+        // let counter = 0;
+        // return {
+            // next() {
+                // if(counter < num) return {value: counter++,done: false};
+                // else return {value: counter, done: true};
+            // }
+        // }
+    // }
+// }
+// function getValue() {
+    // for(let i of objectIterator) {
+        // console.log(i);
+    // }
+// }
+// getValue();
+
 const objectIterator = {
-    arr: ['first',
-            'second',
-            'third'  ],
-    methodGenerator(array = this.arr) {
+    number: 10,
+    name: 'John',
+    surname: 'Brown',
+    [Symbol.iterator]() {
+        const arrayKeys = Object.keys(this);
+        const limit = arrayKeys.length;
         let counter = 0;
+        const _this = this;
         return {
             next() {
-                if(counter < array.length) return {value: array[counter++],done: false};
-                else return {value: array[counter],done: true};
+                if(counter < limit)return {value: _this[arrayKeys[counter++]],done: false};
+                else return {value: _this[arrayKeys[counter]], done: true};
             }
         }
-    },
-    getMethodGenerator(arr = this.arr) {
-        iterator = objectIterator.methodGenerator(arr);
-        for(let i = 0; i < arr.length; i++) {
-            console.log(iterator.next().value);
-        }
-    return console.log(iterator.next());
+    }
 }
+function getValue(object) {
+    for(let i of object) {
+        console.log(i);
+    }
 }
-const arr = [
-    'first',
-    'second',
-    'third',
-    'four',
-    'fifth'
-];
-objectIterator.getMethodGenerator();
+getValue(objectIterator);
