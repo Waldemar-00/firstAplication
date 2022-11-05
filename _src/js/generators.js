4ba9335336229fe06b3f1f9db11cbc39ba0cdff8
waldemar <vova108vova@gmail.com>
2022-11-05 17:39:24 +0300

    methodGenerator

--- _src/js/generators.js
+++ _src/js/generators.js
@@ -1,3 +1,4 @@
+/*
 function* generator(a, b, c, d, e) {
     // yield a;
     // yield b;
@@ -23,4 +24,35 @@ console.log(iteration.next().value);
 console.log(iteration.next().value);
 console.log(iteration.next());// terminal condition is Infinity => done: true
 console.log(iteration.next());// terminal condition is Infinity => done: true
+*/
+//? GENERATOR
 
+const objectIterator = {
+    arr: ['first',
+            'second',
+            'third'  ],
+    methodGenerator(array = this.arr) {
+        let counter = 0;
+        return {
+            next() {
+                if(counter < array.length) return {value: array[counter++],done: false};
+                else return {value: array[counter],done: true};
+            }
+        }
+    },
+    getMethodGenerator(arr = this.arr) {
+        iterator = objectIterator.methodGenerator(arr);
+        for(let i = 0; i < arr.length; i++) {
+            console.log(iterator.next().value);
+        }
+    return console.log(iterator.next());
+}
+}
+const arr = [
+    'first',
+    'second',
+    'third',
+    'four',
+    'fifth'
+];
+objectIterator.getMethodGenerator();
