a700ee36702b39a789ad92f2f7a82c1a1932a6e5
waldemar <vova108vova@gmail.com>
2022-11-08 20:42:16 +0300

    task added to tasks.js, used Set()

--- _src/js/tasks.js
+++ _src/js/tasks.js
@@ -13,3 +13,14 @@
 // }
 // console.log(amountOfPages(401));
 // 
+
+function isPangram(string) {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const setFromString = new Set(string.split(' ').join('').toUpperCase().split('').sort());
+    const newString = (Array.from(setFromString)).join('');
+    if(newString === alphabet) return true;
+    else return false;
+}
+console.log(isPangram('The quick brown fox jumps over the lazy dog'));
+console.log(isPangram('Hello World'));
+
