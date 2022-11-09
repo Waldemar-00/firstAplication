dfafab6074f94a128682606e888c820de1ad3c6b
waldemar <vova108vova@gmail.com>
2022-11-09 20:50:33 +0300

    recursion task

--- _src/js/tasks.js
+++ _src/js/tasks.js
@@ -14,13 +14,36 @@
 // console.log(amountOfPages(401));
 // 
 
-function isPangram(string) {
-    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const setFromString = new Set(string.split(' ').join('').toUpperCase().split('').sort());
-    const newString = (Array.from(setFromString)).join('');
-    if(newString === alphabet) return true;
-    else return false;
-}
-console.log(isPangram('The quick brown fox jumps over the lazy dog'));
-console.log(isPangram('Hello World'));
+// function isPangram(string) {
+    // const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    // const setFromString = new Set(string.split(' ').join('').toUpperCase().split('').sort());
+    // const newString = (Array.from(setFromString)).join('');
+    // if(newString === alphabet) return true;
+    // else return false;
+// }
+// console.log(isPangram('The quick brown fox jumps over the lazy dog'));
+// console.log(isPangram('Hello World'));
 
+ 
+function deepCount(a){
+    let count = 0;
+    const b = a;
+    wrapper(b);
+    function wrapper(b) {
+        b.forEach((item) => {
+            count++;
+        if(Array.isArray(item))wrapper(item);
+    });
+    return count;
+    }
+    return count;
+}
+
+console.log(deepCount([1, 5, 3])); // => 3
+
+console.log(deepCount(["1", 5, "3", ["10"]])); // => 5 (Заметьте, что последний элемент был посчитан сам + его внутренность)
+
+console.log(deepCount([1, 2, [3, 4, [5]]])); // => 7
+
+console.log(deepCount([])); // => 0
+console.log(deepCount([[[[[[[[[]]]]]]]]])); //=> 8
