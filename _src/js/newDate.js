018b4b637a921f98beb88a589e64a63c02676432
waldemar <vova108vova@gmail.com>
2022-11-15 21:23:08 +0300

    get, set of Date object, benchmark

--- _src/js/newDate.js
+++ _src/js/newDate.js
@@ -13,18 +13,56 @@
 // console.log(sameAsBigDay.setTime(theBigDay.getTime()));
 // console.log(sameAsBigDay.setTime(theBigDay));
 
-const time = new Date(); //! it is object, has methods
-console.log(time.getFullYear());
-console.log(time.getMonth());
-console.log(time.getDate());
-console.log(time.getDay());
-console.log(time.getHours());
-console.log(time.getMinutes());
-console.log(time.getSeconds());
-console.log(time.getMilliseconds());
-console.log('______________________')
-console.log(time + ' Time');
+// const time = new Date(); //! it is object, has methods
+// console.log(time.getFullYear());
+// console.log(time.getMonth());
+// console.log(time.getDate());
+// console.log(time.getDay());
+// console.log(time.getHours());
+// console.log(time.getMinutes());
+// console.log(time.getSeconds());
+// console.log(time.getMilliseconds());
+// console.log('______________________')
+// console.log(time + ' Time');
+// 
+// const timeNew = Date(); //! it is a string, no methods!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// console.log(timeNew + ' newTime');
+// console.log(timeNew.getFullYear);
 
-const timeNew = Date(); //! it is a string, no methods!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-console.log(timeNew + ' newTime');
-console.log(timeNew.getFullYear);
+// const time = new Date();
+// console.log(time);
+// console.log(time.setFullYear(2010));
+// console.log(time.setUTCFullYear(2010));
+// console.log(new Date().setUTCFullYear(2010));
+// console.log(time.setTime(2010));
+const timeStart = Date.now();
+function times(n) {
+    let str = '';
+    for(let i = 0; i < n; i++) {
+            str += i;
+    }
+    return str;
+}
+times(20000);
+const timeEnd = Date.now();
+function timeFor(start, end) {
+    const rezalt = end - start;
+    return rezalt;
+}
+const rezalt_1 = timeFor(timeStart, timeEnd);
+console.log(rezalt_1);
+
+console.log(new Date(0)); //! milliseconds
+console.log(new Date(-777777777777));
+console.log(new Date(1945, 5, 9, 22, 37, 2));
+console.log('1945-05-09T22:37:02.223Z');
+console.log(new Date().getHours());
+console.log(new Date().getUTCHours());//! UTC +0
+
+console.log(new Date().getTimezoneOffset());//! in minutes
+console.log(new Date().getTime());//!milliseconds from 1.01.1970
+console.log(Date());//! full date in string
+console.log(typeof Date());
+const newTime = new Date();
+newTime.setHours(17); //! stands on 17 hours, but VSC return in UTC - 14 => ................Z
+console.log(newTime);
