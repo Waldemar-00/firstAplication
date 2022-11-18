b6d92706d6224860988e3f85baf8559b508b9946
waldemar <vova108vova@gmail.com>
2022-11-18 20:36:57 +0300

    parametrs JS

---
+++ parametres_JS/preview/script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const box = document.querySelector('.box');
+// const width = box.clientWidth;
+// const height = box.clientHeight;
+// console.log(width, height);
+// const offsetWidth = box.offsetWidth;
+// const offsetHeight = box.offsetHeight;
+// console.log(offsetWidth, offsetHeight);
+// const boxScroll = box.scrollHeight;
+// const boxScrollWidth = box.scrollWidth;
+// console.log(boxScroll);
+// console.log(boxScrollWidth);
+// 
+const button = document.querySelector('button');
+// function changeHeightBox() {
+    // box.classList.add('box_fullHeight');
+// }
+// button.addEventListener('click', changeHeightBox); 
+
+// function changeHeightBox_1() {
+    // box.style.height = box.scrollHeight + 'px';
+// }
+// button.addEventListener('click', changeHeightBox_1);
+
+// function getHeightScroll() {
+    // console.log(box.scrollTop + 'px');
+// }
+// button.addEventListener('click', getHeightScroll);
+// console.log('__________________________________');
+// const parameters = box.getBoundingClientRect();
+// console.log(parameters.bottom);
+// console.log(parameters.top);
+// console.log(parameters.left);
+// console.log(parameters.right);
+// console.log(parameters.width);
+// console.log(parameters.height);
+// console.log(parameters);
+// console.log(parameters.x);
+// console.log(parameters.y);
+// const computedStyle = getComputedStyle(box);
+// console.log(computedStyle.display);
+// console.log(computedStyle);
+// console.log(computedStyle[291]);
+// 
+// 
+console.log(document.documentElement.style);
+console.log(document.documentElement.scrollTop);
+console.log(document.documentElement.clientWidth);
+console.log(document.documentElement.clientHeight);
