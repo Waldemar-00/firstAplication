6214b80b2f669bd2094072022848533b390ca4fd
waldemar <vova108vova@gmail.com>
2022-11-18 23:07:25 +0300

    modal window open-close

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -83,4 +83,28 @@ window.addEventListener('DOMContentLoaded', () => {
         return updateTimer();
     }
     setTimerOnPage('.timer', deadLine);
+    //! modal
+    const buttons = document.querySelectorAll('[data-modal]');
+    const modal = document.querySelector('.modal');
+    const close = document.querySelector('[data-close]');
+    function addClassShow(event) {
+        if(event.target && event.target.matches('[data-modal]')) {
+            modal.classList.add('show', 'fade');
+            modal.classList.remove('hide');
+            document.body.classList.add('hidden');
+        }
+    }
+    function addClassHide(event) {
+        if((event.target && event.target.matches('[data-close]')) || (event.target && event.target.matches('.modal')) || (event.target && event.code === 'Escape' && modal.classList.contains('show'))) {
+            modal.classList.remove('show');
+            document.body.classList.remove('hidden');
+            modal.classList.add('hide');
+        }
+    }
+    buttons.forEach(btn => {
+        btn.addEventListener('click', addClassShow);
+    });
+    close.addEventListener('click', addClassHide);
+    modal.addEventListener('click', addClassHide);
+    document.addEventListener('keydown', addClassHide);
 });
