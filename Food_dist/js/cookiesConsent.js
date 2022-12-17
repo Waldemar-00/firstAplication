3ed0c1563c35623c5772783ae2d27112ce84dfc4
waldemar <vova108vova@gmail.com>
2022-12-17 17:33:11 +0300

    Cookies in cookiesStorage, made object for cookiesStorage with methods getItem & setItem

--- Food_dist/js/cookiesConsent.js
+++ Food_dist/js/cookiesConsent.js
@@ -21,9 +21,23 @@
     // }
 // });
 // 
+
+//! COOKIE STORAGE
 window.addEventListener('DOMContentLoaded', () => {
-    const shouldShowPopup = () => localStorage.getItem('site_consent') === 'true'? true : false;//finding property in window.localStorage, return value
-    const toggleToLocalStorage = (value) => localStorage.setItem('site_consent', value);
+    const cookieStorage = {
+        getItem: () => {
+            const cookies = document.cookie.split(';').map(cookie => cookie.split('=')).reduce((acc, [key, value]) => (
+                {...acc, [key.trim()] : value}), {});
+                console.log(cookies);
+                return cookies;
+        },
+        setItem: (key, value) => {
+            document.cookie = `${key}=${value};expires=Sun, 17 Apr 2023 00:00:00 GMT`;
+        },
+    };
+    
+    const shouldShowPopup = () => cookieStorage.getItem().site_consent === 'true' ? true : false;//finding property in window.localStorage, return value
+    const toggleToLocalStorage = (value) => cookieStorage.setItem('site_consent' , value);
 
     const popup = document.querySelector('.popup');
     const buttonCansel = document.querySelector('[data-cancel]');
@@ -36,9 +50,11 @@ window.addEventListener('DOMContentLoaded', () => {
             popup.classList.remove('popup_active');
             console.log('Something loading...');
         });
+
         buttonCansel.addEventListener('click', () => {
             toggleToLocalStorage(false);
             popup.classList.remove('popup_active');
+            console.log('Cansel clicked');
         });
     }
 });
